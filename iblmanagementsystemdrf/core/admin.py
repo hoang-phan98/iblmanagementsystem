20307ee366d35560223253fcd73a3cb9473a1517
Kenny Ye <kenny-ye@hotmail.com>
2020-06-13 19:41:45 +1000

    added admin functionality - will be able to create objects now

--- iblmanagementsystemdrf/core/admin.py
+++ iblmanagementsystemdrf/core/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from core.models import Course, Interview, Supervisor, Student, Company, Placement, Unit, UnitCourse, Application
 
 # Register your models here.
+# class ProductAdmin(admin.ModelAdmin):
+#     readonly_fields = ['id', 'slug']
+# admin.site.register(Product, ProductAdmin)
+
+admin.site.register(Course)
+admin.site.register(Interview)
+admin.site.register(Supervisor)
+admin.site.register(Student)
+admin.site.register(Company)
+admin.site.register(Placement)
+admin.site.register(Unit)
+admin.site.register(UnitCourse)
+admin.site.register(Application)
